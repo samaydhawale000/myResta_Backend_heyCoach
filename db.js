2f7a9ffd7aeccd038245fa86efa9601f05286d9a
Samay Dhawale <samaydhawale1@gmail.com>
2023-09-17 16:55:54 +0530

    db changes

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const newCon = mysql.createConnection({
   connectionLimit: 10,
-  host: "localhost",
+  host: "10.101.10.186",
   user: "root",
   password: "",
   database: "restaurant_backend",
